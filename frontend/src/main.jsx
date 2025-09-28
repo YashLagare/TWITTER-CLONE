841a250641a446718ffcf57d16b7d6553c3a6b09
YashLagare <yashlagare77@gmail.com>
2025-09-28 21:00:50 +0530

    auth handle and added protected route in client

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,25 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+const queryClient = new QueryClient({
+  //this required cause whenever we switch tab it send request by default so we have to add this 
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 )
